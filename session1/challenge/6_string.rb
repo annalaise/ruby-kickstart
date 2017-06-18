fd75fe628871519dbd06ca66e86a6661d1cfde99
annalaise <annalaise.gibbons@gmail.com>
2017-06-18 09:37:54 +0800

    adding comments to explain understanding of solution for exercises 6 and 7

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -7,12 +7,21 @@
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
+# the line below sets the method name and the arguments that are passed to it
 def odds_and_evens(string, return_odds)
+# this is setting the return_string variable to an empty string, so a new string can be passed into it
   return_string = ""
+# this takes the string argument passed to it, and calls the size method on it, giving the strings length.
+# This number is then passed to the times method, telling the next set of instructions how many times to loop.
+# and then, it's asking for each character in the string to be assigned 'index'
   string.size.times do |index|
+# the first next if statement is saying 'If return_odds boolean and index.even are both true, skip this'
     next if return_odds && index.even?
+# the second next if statement is saying 'if return_odds and index.odd are not the same, skip this too'
     next if !return_odds && index.odd?
+# this then inserts the remaining string index into the return_string empty string
     return_string << string[index]
   end
+# this then returns what is in the return_string variable after the method has completed its work
   return_string
 end
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -4,13 +4,21 @@
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
+# this line below sets the method name and the singular argument passed to it (a string)
 def pirates_say_arrrrrrrrr(string)
+# this line creates a variable called return_string that will take a string passed to it
   return_string = ""
+# this sets next_char variable to be false (which is later set as true if a condition is met)
   next_char = false
+# this looks at the size of the string, and tells Do to index it by the number of characters
   string.size.times do |index|
+# this sets current_char variable to equal the index of the string
     current_char = string[index]
+# this inserts the current_char into return_string only if next_char is true
     return_string << current_char if next_char
+# and here is the condition to that makes next_char true - if current_char is equal to "r" or "R"
     next_char = (current_char == "r" || current_char == "R")
   end
+# this returns the result of return_string once the method has carried out its work
   return_string
-end 
+end
