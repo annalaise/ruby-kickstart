85a3142f1ddeb3863a6b22e328ca2b043807ff28
annalaise <annalaise.gibbons@gmail.com>
2017-06-20 17:08:43 +0800

    completed exercise 4, session 2, with notes explaining what the method is doing

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,9 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(x)
+# the line below calls select on the array argument passed to get_squares. This is then passed into the
+# block as an input, and asks if the array includes the number squared. This is then sorted.
+  x.select { |n| x.include? n*n }.sort
+end
