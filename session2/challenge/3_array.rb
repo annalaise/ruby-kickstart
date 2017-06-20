119a347f63dace51d935bb6932b1ad578203a3a5
annalaise <annalaise.gibbons@gmail.com>
2017-06-20 16:58:15 +0800

    completed exercise 3, session 2, with added notes to understand the given solution

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,5 +7,14 @@
 
 class String
   def every_other_char
+    string_array = ""
+# the line below calls the each_char string method, which passes each character in a string to the given
+# block. with_index then indexes each character. The line then initialises a block and sets char
+# and index as the objects it is being passed.
+    each_char.with_index do |char, index|
+# this line inserts char into the string_array if the index is even.
+    string_array << char if index.even?
+    end
+    string_array
   end
 end
