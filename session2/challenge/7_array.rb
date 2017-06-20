98d4eefc7d659356d04b65e46e79f2bb23776d73
annalaise <annalaise.gibbons@gmail.com>
2017-06-20 20:31:33 +0800

    completed exercise 7 with explantory notes about solution

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,26 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(sentence)
+# this line takes all the chars that are to be excluded, splits them and assigns them to char, then
+# substitutes them with a blank space.
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each do |char|
+    sentence = sentence.gsub(char, ' ')
+  end
+# this line takes sentence, splits it into a new array, and assigns the array to words
+  words = sentence.split
+# this is an empty array, waiting to be filled:
+  return_to = []
+# this line takes the words array, splits it and assigns each object an index number
+    words.each_with_index do |word, index|
+# this line adds the word into the return_to array if the index number is even
+    return_to << word if index.even?
+    end
+    return_to
+  end
